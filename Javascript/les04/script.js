09d92e18082b15b577f399dd56a0787edc0b25a6
Lua <jussyman13@gmail.com>
2022-01-19 22:56:32 +0100

    revisions and new lessons

---
+++ Javascript/les04/script.js
@@ -0,0 +1,36 @@
+const myTitle = document.getElementById('myTitle');
+const myImage = document.getElementById('myImage');
+
+let seizoenen = [
+    {
+        "titel":"seizoenen",
+        "image":"img/seasons.jpg",
+        "backGround":"gray"
+    },
+    {
+        "titel":"lente",
+        "image":"img/spring.jpg",
+        "backGround":"green"
+    },
+    {
+        "titel":"zomer",
+        "image":"img/summer.jpg",
+        "backGround":"blue"
+    },
+    {
+        "titel":"herfst",
+        "image":"img/autumn.jpg",
+        "backGround":"orange"
+    },
+    {
+        "titel":"winter",
+        "image":"img/winter.jpg",
+        "backGround":"white"
+    }
+];
+
+function show(index){
+    myTitle.innerHTML = seizoenen[index].titel;
+    myImage.src = seizoenen[index].image;
+    document.body.style.backgroundColor = seizoenen[index].backGround;
+}
